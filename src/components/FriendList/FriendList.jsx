5340d08a2359c73bac42517766c6159a40f25c42
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-01-20 23:38:03 +0100

    modul1 react

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+import { FriendListItem } from "../FriendListItem/FriendListItem";
+import css from "./FriendList.module.css";
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ isOnline, id, avatar, name }) => {
+        return (
+          <li
+            className={css.friendDiscription}
+            key={id}
+          >
+            <FriendListItem
+              avatar={avatar}
+              name={name}
+              isOnline={isOnline}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,38 +1,33 @@
-import css from './Profile.module.css';
-import PropTypes from 'prop-types';
+import css from "./Profile.module.css";
 
-export const Profile = ({ username, tag, location, avatar, stats }) => {
+export const Profile = ({ name, avatar, tag, location, stats }) => {
   return (
     <div className={css.profile}>
       <div className={css.description}>
-        <img src={avatar} alt={username} width="300" className={css.avatar} />
-        <p className={css.name}>{username}</p>
+        <img
+          src={avatar}
+          alt={name}
+          width="150"
+          className={css.hiroAvatar}
+        />
+        <p className={css.name}>{name}</p>
         <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
-
-      <ul className={css.stats}>
-        <li>
+      <ul className={css.statslist}>
+        <li className={css.statsColomn}>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
+          <span className={css.value}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.statsColomn}>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
+          <span className={css.value}>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.statsColomn}>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
+          <span className={css.value}>{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
-};
-
-Profile.propTypes = {
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  stats: PropTypes.object.isRequired,
 };
